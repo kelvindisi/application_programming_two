b205e14c7edd554a1f33fa42a2b756064c0d70d0
Kelvin Ndisi <kelvindisi@yahoo.com>
2021-10-26 20:10:20 +0300

    abstract classes

---
+++ AbstractClassesExample/AbstractClassesExample/Person.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AbstractClassesExample
+{
+    abstract class Person
+    {
+        protected string name;
+        protected int age;
+
+        public abstract void getDetails();
+        public abstract void printDetails();
+    }
+}
---
+++ AbstractClassesExample/AbstractClassesExample/Staff.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AbstractClassesExample
+{
+    class Staff: Person
+    {
+        private string department;
+
+        public override void getDetails()
+        {
+            Console.Write("Enter full name: ");
+            name = Console.ReadLine();
+            Console.Write("Enter age: ");
+            age = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Enter deparment: ");
+            department = Console.ReadLine();
+        }
+
+        public override void printDetails()
+        {
+            Console.WriteLine("Staff name is {0} who is {1} years old works at {2} department", name, age, department);
+        }
+    }
+}
