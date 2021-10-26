eb26de210e50b2533f1fbca7a7c03e069493b35e
Kelvin Ndisi <kelvindisi@yahoo.com>
2021-10-26 20:22:50 +0300

    interfaces examples

---
+++ InterfacesExamples/InterfacesExamples/Course.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InterfacesExamples
+{
+    class Course: ICourse
+    {
+        private int courseId;
+        private string courseName;
+
+        public int PCourseId
+        {
+            get { return courseId; }
+            set { courseId = value; }
+        }
+        public string PCourseName
+        {
+            get { return courseName; }
+            set { courseName = value; }
+        }
+
+        public void getDetails()
+        {
+            Random rand = new Random();
+            PCourseId = rand.Next(100, 500);
+            Console.Write("Enter course name: ");
+            PCourseName = Console.ReadLine();
+        }
+        public void printDetails()
+        {
+            Console.WriteLine("Course ID: {0} Course name: {1}", PCourseId, PCourseName);
+        }
+    }
+}
---
+++ InterfacesExamples/InterfacesExamples/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InterfacesExamples
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Course course = new Course();
+            course.getDetails();
+            course.printDetails();
+
+            Console.ReadKey();
+        }
+    }
+}
